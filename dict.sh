5f659a562eab5b4b48dff2ef8642b7b59eb7179d
Alexander Lopatin <sbar.geek@gmail.com>
2011-01-13 15:03:16 +0300

    interactive input added

--- dict.sh
+++ dict.sh
@@ -8,12 +8,25 @@
 # the Free Software Foundation, either version 3 of the License, or (at
 # your option) any later version.
 
-# TODO: show "did you mean" words if available
-
-STR=$(echo -n $@ | sed -e 's/ /%20/g')
-URL="http://www.google.com/dictionary?q=$STR\
+dict()
+{
+    STR=$(echo -n $@ | sed -e 's/ /%20/g')
+    URL="http://www.google.com/dictionary?q=$STR\
 &hl=ru&langpair=en|ru&spell=1&oi=spell"
-wget -qO - $URL |
-    egrep 'span.*dct-..">.*(<\/span>|)$' |
-    sed -e 's/<span class="dct-..">//g;s/<\/span>//g;s/&nbsp;/ /g' |
-    egrep -v '(dct-elb|a href)'
+    wget $URL -qO - |
+        egrep 'span.*dct-..">.*(<\/span>|)$' |
+        sed -e 's/<span class="dct-..">//g;s/<\/span>//g;s/&nbsp;/ /g' |
+        egrep -v '(dct-elb|a href|object)'
+}
+
+if [[ $1 == '' ]]; then
+    while :
+    do
+        echo -n 'google dict: '
+        read TEXT
+        [[ $TEXT == '' ]] && break
+        dict "${TEXT}"
+    done
+else
+    dict $@
+fi
