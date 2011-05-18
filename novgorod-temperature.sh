ff88a8eebc68dd7a41623ecadd8464bcb5b39839
Alexander Lopatin <sbar.geek@gmail.com>
2011-05-18 19:03:22 +0400

    timeout decreased

--- novgorod-temperature.sh
+++ novgorod-temperature.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # shows temperature in Velikij Novgorod town
 
-curl -s http://www.novgorod.ru/temperature/ \
+curl --connect-timeout 5 -s http://www.novgorod.ru/temperature/ \
     | iconv -f cp1251 | grep -io "сейчас:\ .*C" | cut -d' ' -f2
