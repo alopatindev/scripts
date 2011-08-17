d99e15ae1140a016ae2789a44d7a180080155c68
Alexander Lopatin <sbar.geek@gmail.com>
2011-08-18 02:44:33 +0400

    replaced mplayer with ffprobe

--- mlen.sh
+++ mlen.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# shows movies' length in human-readable format using mplayer
+# Shows movies' duration in human-readable format
 
-LEN=$(mplayer -vo null -ao null -frames 0 -identify "$@" \
-    | grep ID_LENGTH \
-    | sed 's/ID_LENGTH=//')
-MIN=$(echo "${LEN} / 60.0" | bc -l)
-INT_HR=$(echo "${MIN} / 60" | bc)
-MINMOD=$(echo "${MIN} - (${INT_HR} * 60)" | bc -s)
-printf "%d hours, %2.f minutes\n" "${INT_HR}" "${MINMOD}"
+ffprobe "$@" 2>>/dev/stdout | grep Duration: | cut -d: -f2,3,4 | cut -d, -f1
