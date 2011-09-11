37a4f619d4ff800949815bb96cc953059efb1d38
Alexander Lopatin <sbar.geek@gmail.com>
2011-09-11 15:55:59 +0400

    shows '+' if the temperature is positive

--- novgorod-temperature.sh
+++ novgorod-temperature.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # shows temperature in Velikij Novgorod town
 
-wget -qO - --timeout=5 http://www.novgorod.ru/weather \
+TEMP=$(wget -qO - --timeout=5 http://www.novgorod.ru/weather \
     | iconv -f cp1251 \
     | grep -io 'Фактическая</td><td>.* °C</td>' \
-    | sed 's/Фактическая<\/td><td>//g;s/<\/td>//g'
+    | sed 's/Фактическая<\/td><td>//g;s/<\/td>//g')
+echo ${TEMP:0:1} | egrep -q '^[0-9]' && TEMP="+${TEMP}"
+echo ${TEMP}
