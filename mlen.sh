5cba213254930450611b6131a267d33fd499f66c
Alexander Lopatin <sbar.geek@gmail.com>
2011-11-02 01:47:18 +0400

    supports more than one file now

--- mlen.sh
+++ mlen.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-# Shows movies' duration in human-readable format
 
-ffprobe "$@" 2>>/dev/stdout | grep Duration: | cut -d: -f2,3,4 | cut -d, -f1
+# shows movies' length in human-readable format using mplayer
+
+human_len () {
+    HH=$(echo "$1 / 3600" | bc)
+    MM=$(echo "($1 - $HH*3600) / 60" | bc)
+    SS=$(echo "$1 - $HH*3600 - $MM*60" | bc)
+    printf '%02d:%02d:%02.02f' $HH $MM $SS
+}
+
+total=0
+for i in "$@"; do
+    LEN=$(mplayer -vo null -ao null -frames 0 -identify "${i}" \
+        | grep ID_LENGTH | sed 's/ID_LENGTH=//')
+
+    human_len $LEN
+    echo " $i"
+    total=$(echo "$total + $LEN" | bc)
+done
+
+echo
+echo -n 'Total:  '
+human_len $total
