ae189893c35afe805e67951c129b6b8e7aecf711
Alexander Lopatin <sbar.geek@gmail.com>
2012-03-10 18:23:52 +0400

    one more spike

--- novgorod-temperature.sh
+++ novgorod-temperature.sh
@@ -3,6 +3,8 @@
 
 TEMP=$(wget -qO - --timeout=5 http://www.novgorod.ru/weather \
     | iconv -f cp1251 \
+    | head -n453 \
+    | tail -n1 \
     | grep -io 'Фактическая</td><td>.* °C</td>' \
     | sed 's/Фактическая<\/td><td>//g;s/<\/td>//g')
 echo ${TEMP:0:1} | egrep -q '^[0-9]' && TEMP="+${TEMP}"
