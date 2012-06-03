3ddab93c1674f1778f84a91340f488252df33c46
Alexander Lopatin <sbar.geek@gmail.com>
2012-06-03 22:28:48 +0400

    bugfix

--- novgorod-temperature.sh
+++ novgorod-temperature.sh
@@ -3,9 +3,7 @@
 
 TEMP=$(wget -qO - --timeout=5 http://www.novgorod.ru/weather \
     | iconv -f cp1251 \
-    | head -n449 \
-    | tail -n1 \
-    | grep -io 'Фактическая</td><td>.* °C</td>' \
+    | grep -aio 'Фактическая</td><td>.* °C</td>' \
     | sed 's/Фактическая<\/td><td>//g;s/<\/td>//g')
 echo ${TEMP:0:1} | egrep -q '^[0-9]' && TEMP="+${TEMP}"
 echo ${TEMP}
